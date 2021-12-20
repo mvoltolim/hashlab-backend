005f156d34332695449c3a686094cdcd55c1b437
Marcus Voltolim <marcus.voltolim@gmail.com>
2021-12-20 15:10:21 -0300

    first commit, create project structure

---
+++ src/main/groovy/com/hash/hashlab/HashlabApplication.groovy
@@ -0,0 +1,13 @@
+package com.hash.hashlab
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HashlabApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(HashlabApplication, args)
+    }
+
+}
---
+++ src/main/java/com/hash/hashlab/HashlabApplication.java
@@ -0,0 +1,13 @@
+package com.hash.hashlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashlabApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(HashlabApplication.class, args);
+    }
+
+}
